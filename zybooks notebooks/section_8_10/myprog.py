082dbe144609778b774ff1d7445ccf2057efbfa9
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-03-25 02:27:10 +0000

    🔄 autosave @ 02:27:10

--- zybooks notebooks/section_8_10/myprog.py
+++ zybooks notebooks/section_8_10/myprog.py
@@ -1,12 +1,11 @@
 import sys
 
-    if len(sys.argv) != 3:
-        print("Usage: python myprog.py name age")
-        sys.exit(1)
+if len(sys.argv) != 3:
+    print("Usage: python myprog.py name age")
+    sys.exit(1)
 
-    name = sys.argv[1]
-    age = int(sys.argv[2])
+name = sys.argv[1]
+age = int(sys.argv[2])
 
-    print(f"Hello {name}.")
-    print(f"{age} is a great age.")
-    
+print(f"Hello {name}.")
+print(f"{age} is a great age.")
