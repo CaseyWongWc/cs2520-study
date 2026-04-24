b7a05d0923e2a71333b40971e2441db365dddf47
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-04-24 04:52:43 +0000

    🔄 autosave @ 04:52:43

---
+++ UnOrdered/METESTINGFOLDER_4_23_2026 two/demo/ohokay.py
@@ -0,0 +1,12 @@
+class MyClass:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+    def add(self):
+        return self.x + self.y
+    def add1(self,  some):
+        return some.x + some.y
+a=MyClass()
+a.x = 5
+a.y = 10
+print(a.add())  # Output: 15
