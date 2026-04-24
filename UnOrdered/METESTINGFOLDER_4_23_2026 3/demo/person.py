60ec4ee24945d67d09ae1b6784e6bbb152ad49fd
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-04-24 07:30:34 +0000

    🔄 autosave @ 07:30:34

---
+++ UnOrdered/METESTINGFOLDER_4_23_2026 3/demo/person.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name="Unknown", age=1):
+        self.__name = name
+        self.__age = age
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, new_name):
+        self.__name = new_name
