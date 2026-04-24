6d6d202246bc127b1fe82d5f337f38047773ea1b
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-04-24 06:49:57 +0000

    🔄 autosave @ 06:49:57

--- UnOrdered/METESTINGFOLDER_4_23_2026 3/demo/demo.py
+++ UnOrdered/METESTINGFOLDER_4_23_2026 3/demo/demo.py
@@ -1,5 +1,5 @@
-from test_input import setin
-setin("Alice", "30")
+#from test_input import setin
+#setin("Alice", "30")
 from demo.person import Person
 person = Person(input("Enter name: "), input("Enter age: "))
 print(person.greet())
