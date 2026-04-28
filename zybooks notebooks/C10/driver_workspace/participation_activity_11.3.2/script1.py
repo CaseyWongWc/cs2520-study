9c8c17c4c21312a0d46e2509ad0eb17ee59c09fa
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-04-28 09:09:45 +0000

    🔄 autosave @ 09:09:45

--- zybooks notebooks/C10/driver_workspace/participation_activity_11.3.2/script1.py
+++ zybooks notebooks/C10/driver_workspace/participation_activity_11.3.2/script1.py
@@ -1,4 +1,4 @@
-from my_funcs import factorial
+from my_funcs import factorial, squared
 a = 5
 print(f"a! = {factorial(a)}")
-print(f"a^2 = {squared(a)}")
+print(f"a^2 = {squared(a)}") # This line will cause an error because squared is not defined in this file.
