3fa6868a0a5bf22dffc310cdc0d8ef45590e5c88
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-04-28 09:03:15 +0000

    🔄 autosave @ 09:03:15

--- zybooks notebooks/C10/driver_workspace/try_11.3.1/main.py
+++ zybooks notebooks/C10/driver_workspace/try_11.3.1/main.py
@@ -1,7 +1,6 @@
 from hashlib import md5, sha1
-text = input("Enter text to hash ("q" to quit): ")
-algorithm = input("
-Enter algorithm (md5/sha1/sha224): ")
+text = input("Enter text to hash (\"q\" to quit): ")
+algorithm = input("\nEnter algorithm (md5/sha1/sha224): ")
 if algorithm == "md5":
     output = md5(text.encode("utf-8"))
 elif algorithm == "sha1":
@@ -10,5 +9,4 @@ elif algorithm == "sha224":
     output = sha224(text.encode("utf-8"))
 else:
     output = "Invalid algorithm selection"
-print(f"
-Hash value: {output.hexdigest()}")
+print(f"\nHash value: {output.hexdigest()}")
