3d1f340918387811084f6e06c0f3c22040b9d558
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-04-29 02:48:25 +0000

    🔄 autosave @ 02:48:25

--- zybooks notebooks/C10/driver_workspace/participation_activity_11.7.1/main.py
+++ zybooks notebooks/C10/driver_workspace/participation_activity_11.7.1/main.py
@@ -1,22 +1,15 @@
-import random
-# Create a shuffled card deck with 4 suites of cards 2-10, and face cards
-deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"] * 4
-random.shuffle(deck)
-num_drawn = 0
-game_over = False
-user_input = input('Press any key to draw a card ("q" to quit): ')
-while user_input != "q" and not game_over:
-    # Draw a random card, and remove card from the deck
-    card = random.choice(deck)
-    deck.remove(card)
-    num_drawn += 1
-    print(f"\nCard drawn: {card}")
-    # Game is over if an ace was drawn
-    if card == "A":
-        game_over = True
-    else:
-        user_input = input('Press any key to draw a card ("q" to quit): ')
-if user_input == "q":
-    print("\nGame was quit")
-else:
-    print(f"{num_drawn} card(s) were drawn to find an ace.")
+import datetime
+
+# Create a new date object representing the current date (May 30, 2016)
+today  = datetime.date.today()
+
+days_from_now = int(input("Enter number of days from now: "))
+
+# Create a new timedelta object that represents a difference in the 
+# number of days between dates.
+day_difference = datetime.timedelta(days = days_from_now)
+
+# Calculate new date
+future_date = today + day_difference
+
+print(f"{days_from_now} days from now is {future_date.strftime('%B %d, %Y')}")
